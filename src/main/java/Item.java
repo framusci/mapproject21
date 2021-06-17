0d654a56294cb7b827c962b5a3ef1e02dab59794
Francesco Musci <francesco.musci5@gmail.com>
2021-06-17 13:48:57 +0200

    Added resources and removed package

---
+++ src/main/java/Item.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author franc
+ */
+public class Item {
+    private String name;
+    private String description;
+    
+    public Item(String name, String description, Item combineItem){
+        this.name = name;
+        this.description = description;
+    }
+}
